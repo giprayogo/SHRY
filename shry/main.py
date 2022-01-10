206e2c82715934fe99784f760f3d2ffcde4883eb
Kosuke Nakano <kousuke_1123@icloud.com>
2022-01-10 18:40:18 +0900

    __version__=1.0.x_devel

--- shry/main.py
+++ shry/main.py
@@ -7,7 +7,7 @@ __copyright__ = "Copyright (c) 2021-, The SHRY Project"
 __credits__ = ["Genki Prayogo", "Kosuke Nakano"]
 
 __license__ = "MIT"
-__version__ = "1.0.3"
+__version__ = "1.0.x_devel"
 __maintainer__ = "Genki Prayogo"
 __email__ = "g.prayogo@icloud.com"
 __date__ = "15. Nov. 2021"
